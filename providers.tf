14725b8eb18189c635192be8a28540803d8efea8
amat-yannamtf <118105695+amat-yannamtf@users.noreply.github.com>
2022-11-15 15:51:04 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -12,7 +12,7 @@ terraform {
 provider "aws" {
    access_key  = "${var.accesskey}"
    secret_key  = "${var.secretkey}"
-   //region      = "${var.region}"
+   region      = "${var.region}"
 }
 
   #The configuration for the remote backend.
