36949dd4c4142bd1df9a085d60efe6d7a0d3d28f
amat-yannamtf <srikanth.yannam@gmail.com>
2022-11-15 19:05:36 +0530

    syntax fixes

--- modules/azurevnetdbdeploy/vnet.tf
+++ modules/azurevnetdbdeploy/vnet.tf
@@ -1,5 +1,5 @@
 resource "azurerm_virtual_network" "amatvnet" {
-    name                = "amat"
+    name                = "amatvnet"
     resource_group_name = local.resource_group_name
     address_space       = [var.vnet_range]
     location            = var.azregion
@@ -15,7 +15,7 @@ resource "azurerm_virtual_network" "amatvnet" {
 
 resource "azurerm_subnet" "subnets" {
     resource_group_name     = local.resource_group_name
-    virtual_network_name    = azurerm_virtual_network.amat.name
+    virtual_network_name    = azurerm_virtual_network.amatvnet.name
     address_prefixes        = [cidrsubnet(var.vnet_range,8,count.index)]
     name                    = var.subnet_names[count.index]
     count                   = length(var.subnet_names)
