d900271856d861ed23333013db8a7a0c05f4c877
amat-yannamtf <118105695+amat-yannamtf@users.noreply.github.com>
2022-11-15 15:35:17 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -15,6 +15,19 @@ provider "aws" {
    //region      = "${var.region}"
 }
 
+  The configuration for the `remote` backend.
+    terraform {
+       backend "remote" {
+         # The name of your Terraform Cloud organization.
+         organization = "https://app.terraform.io/app/amat-tf"
+
+         # The name of the Terraform Cloud workspace to store Terraform state files in.
+        workspaces {
+           name = "aws-dev"
+         }
+       }
+     }
+
  terraform {
    backend "s3" {
      bucket = "backendtfamat"
